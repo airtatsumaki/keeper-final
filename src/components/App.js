57ac9a7a6a667cd62c282a355057e51fde1d4f1c
tatsu <naz1mb20@gmail.com>
2023-03-13 20:21:08 +0000

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -6,31 +6,23 @@ import CreateArea from "./CreateArea";
 
 function App() {
   
-  const [notes, setNotes] = useState([{title: "Note title", content: "Note content"}]);
-
-  function addNote(noteObj){
-    // console.log(noteObj);
-    setNotes((prev) => {
-      return [...prev, noteObj];
-    });
-  }
-
-  function deleteNote(noteId){
-    console.log(noteId);
-    // setNotes(prev => {
-    // //   prev.filter((item, index) => index !== noteId);
-    //   return [prev.pop(noteId)];
-    // });
-
-    // setItems(prevItems => prevItems.filter((item, index) => index !== id));
-  }
+  const [notes, setNotes] = useState([]);
 
   return (
     <div>
       <Header />
-      <CreateArea handleAdd={addNote}/>
+      <CreateArea handleAdd={(noteObj) => {
+        setNotes((prev) => [...prev, noteObj]);
+      }}/>
       {notes.map((theNote, index) => {
-        return <Note key={index} id={index} title={theNote.title} content={theNote.content} handleDelete={deleteNote} />
+        return <Note 
+        key={index} 
+        id={index} 
+        title={theNote.title} 
+        content={theNote.content} 
+        handleDelete={(id) => {
+          setNotes(prevItems => prevItems.filter((item, index) => index !== id));
+        }} />
       })}
       <Footer />
     </div>
